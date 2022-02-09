2faabfd1c958ff7977f1e3b8acc0d0081e7fa096
Leonardo Henrique Texeira <82117002+LeonardoHetx@users.noreply.github.com>
2022-02-09 11:37:01 -0300

    Add files via upload

---
+++ interface.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded',() => {
+
+  let squares = document.querySelectorAll(".square")
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick)
+  })
+})
+
+function handleClick(event){
+  let square = event.target
+  let position = square.id
+
+   if (handleMove(position)){
+     setTimeout(()=>{alert(`Jogador ${playerTime} venceu com a sequência ${seq} !`)},10)
+     
+   }
+  updateSquare(position)
+}
+
+function updateSquare(position){
+  let square = document.getElementById(position.toString())
+  let symbol = board[position]
+  square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+button = document.getElementById("btn")
+button.addEventListener("click",restart)
+
+function restart(){
+  board = ["", "", "", "", "", "", "", "", ""]
+  playerTime = 0
+  gameOver = false
+  updateSquares()
+}
+
+function updateSquares(){
+  let squares  = document.querySelectorAll(".square")
+
+  squares.forEach((square)=>{
+    position = square.id
+    symbol = board[position]
+
+    if (symbol == "") {
+      square.innerHTML = `<div class'${symbol}></div>`
+    }
+  })
+}
+
