23336a58a6d826e78443d3611560b80d22d3cea5
Leonardo Henrique Texeira <82117002+LeonardoHetx@users.noreply.github.com>
2022-02-11 16:53:33 -0300

    Update game.js

--- game.js
+++ game.js
@@ -4,14 +4,14 @@ let gameOver = false
 let symbols = ["o", "x"]
 
 let winStates = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
   [1, 4, 7],
   [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
 ]
 
 function handleMove(position) {
@@ -36,9 +36,9 @@ function isWin() {
   for (let i = 0; i < winStates.length; i++) {
     seq = winStates[i]
 
-    let pos1 = seq[0]
-    let pos2 = seq[1]
-    let pos3 = seq[2]
+    let pos1 = seq[0] -1
+    let pos2 = seq[1] -1
+    let pos3 = seq[2] -1
 
     if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != "") {
       return true
