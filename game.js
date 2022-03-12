c7d13a1c91c1ffddb291f5c19a85e0c9ce094fee
Leonardo Henrique Texeira <82117002+LeonardoHetx@users.noreply.github.com>
2022-03-11 22:53:27 -0300

    Update game.js

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ let board = ["", "", "", "", "", "", "", "", ""]
 let playerTime = 0
 let gameOver = false
 let symbols = ["o", "x"]
+let countTurn = 0
 
 let winStates = [
   [1, 2, 3],
@@ -21,13 +22,16 @@ function handleMove(position) {
 
   if (board[position] == "") {
     board[position] = symbols[playerTime]
+    countTurn++
 
     gameOver = isWin()
 
     if (gameOver == false) {
-      playerTime =  (playerTime == 0 ? 1 : 0)
+      playerTime = (playerTime == 0 ? 1 : 0)
     }
+    
   }
+  
   return gameOver
 }
 
@@ -35,14 +39,13 @@ function handleMove(position) {
 function isWin() {
   for (let i = 0; i < winStates.length; i++) {
     seq = winStates[i]
-
-    let pos1 = seq[0] -1
-    let pos2 = seq[1] -1
-    let pos3 = seq[2] -1
+    let pos1 = seq[0] - 1
+    let pos2 = seq[1] - 1
+    let pos3 = seq[2] - 1
 
     if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != "") {
       return true
-    }
+    }   
   }
   return false
 }
